054ff154d0d3882b9b53931166505a5d9d5f6ffd
unknown <mpmartins@bol.com.br>
2022-01-12 16:18:53 -0300

    q
    q

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect, useState} from "react"
+import Reduztexto from './component/reduz';
+var reduzido=true
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  const textoInicial="Olá, tudo bem como vai ? Como está a familia ?"
+  const maxLength=10
+  
+  const horaInicial=new Date().toLocaleTimeString()
+  const [texto,cutText]= useState(<Reduztexto txt={textoInicial} mxl={maxLength}/>)
+  const [botao,changebutton]= useState("AMPLIA")
+  const [hora,mudahora]= useState(horaInicial)
+  const [dados,setArray]= useState([{nome:"Mauro",cidade:"Floripa",idade:"51",filme:"As tranças do careca"}])
+  function mudaTexto(){
+      reduzido=!reduzido
+      if (reduzido){
+          cutText(<Reduztexto txt={textoInicial} mxl={maxLength}/>)
+          changebutton("AMPLIA")
+      }else{
+          changebutton("REDUZ")
+          cutText(textoInicial)
+      } 
+      
+  }
+  function alteraHora(){
+      const novaHora=new Date().toLocaleTimeString()
+      mudahora(novaHora)
+  }
+  
+  setInterval(alteraHora,1000)
 
-export default App;
+
+
+  return <>
+            
+            <div><span>{texto}</span><button onClick={mudaTexto} >{botao}</button> </div>
+            <div><h1>Hora Certa</h1><h3>{hora}</h3> </div>
+            <p>Seu nome : {dados[0].nome}</p>
+            <p>Cidade onde reside : {dados[0].cidade}</p>
+            <p>Sua idade : {dados[0].idade}</p>
+            <p>Filme preferido : {dados[0].filme}</p>
+           </>
+          
+  
+}
+export default App
+
